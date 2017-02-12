3a4a21a37070e4af455013c794e9d24fe06646c8
apbelg <apbelg@mail.ru>
2017-02-12 22:24:14 +0300

    Update about.php

--- about.php
+++ about.php
@@ -1,2 +1,18 @@
 <?php
+$user_name = 'Андрей';
+$my_age = 45;
+$email = 'apbelg@mail.ru';
+$about = 'инженер, программист 1С';
+$city = 'Белгород';
 ?>
+<!Doctype html>
+<html>
+<h1> Страница пользователя  <? echo $user_name ?> </h1>
+<table>
+  <tr><td>Имя</td><td><? echo $user_name ?></td></tr>
+  <tr><td>Возраст</td><td><? echo $my_age ?></td></tr>
+  <tr><td>Электронная почта     </td><td><a href="mailto:<? echo $email ?>"><? echo $email ?></a> </td></tr>
+  <tr><td>Город</td><td><? echo $city ?></td></tr>
+  <tr><td>О себе</td><td><? echo $about ?></td></tr>
+</table>
+</html>
