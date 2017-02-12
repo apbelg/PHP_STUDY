cb08afea0424055e88b70acc254c33a03ba7c748
apbelg <apbelg@mail.ru>
2017-02-12 22:43:18 +0300

    Update about.php

--- about.php
+++ about.php
@@ -1,18 +1,15 @@
 <?php
 $user_name = 'Андрей';
 $my_age = 45;
-$email = 'apbelg@mail.ru';
-$about = 'инженер, программист 1С';
-$city = 'Белгород';
+$email  = 'apbelg@mail.ru';
+$about  = 'инженер-системотехник';
+$city   = 'Белгород';
 ?>
-<!Doctype html>
-<html>
-<h1> Страница пользователя  <? echo $user_name ?> </h1>
+<h1> Страница пользователя  <?php echo($user_name); ?> </h1>
 <table>
-  <tr><td>Имя</td><td><? echo $user_name ?></td></tr>
-  <tr><td>Возраст</td><td><? echo $my_age ?></td></tr>
-  <tr><td>Электронная почта     </td><td><a href="mailto:<? echo $email ?>"><? echo $email ?></a> </td></tr>
-  <tr><td>Город</td><td><? echo $city ?></td></tr>
-  <tr><td>О себе</td><td><? echo $about ?></td></tr>
+  <tr><td>Имя</td><td><?php echo ($user_name); ?></td></tr>
+  <tr><td>Возраст</td><td><?php echo ($my_age); ?></td></tr>
+  <tr><td>Электронная почта     </td><td><a href="mailto:<?php echo ($email); ?>"><?php echo ($email); ?></a> </td></tr>
+  <tr><td>Город</td><td><?php echo ($city); ?></td></tr>
+  <tr><td>О себе</td><td><?php echo ($about); ?></td></tr>
 </table>
-</html>
