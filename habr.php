4e7c99989c1067efb452c3d56a7476c87c8e5e84
apbelg <apbelg@mail.ru>
2017-02-22 11:51:38 +0300

    Update habr.php

--- habr.php
+++ habr.php
@@ -1,5 +1,4 @@
 <?php
-
 // функция вырезания информации
 function our_find ($date_pub,$homepage,$end_str)
 {
@@ -8,13 +7,10 @@ function our_find ($date_pub,$homepage,$end_str)
   $b = mb_strpos($homepage1,$end_str);
   $c = mb_substr($homepage1,0,$b+1);
   $c = trim(strip_tags($c));
-//  $c = str_replace("\n", NULL, $c);
-//  $c = str_replace("\r", NULL, $c);
   return $c;
 }
 
 // Основной модуль
-
 mb_internal_encoding('utf-8');
 $nom_arcticle = rand(1,172223);
 $article = 'https://habrahabr.ru/post/'.$nom_arcticle."/";
@@ -26,50 +22,47 @@ if ($homepage === false)
   echo '<h2>'.'Доступ к публикации закрыт или другая причина ошибки !!!'.'</h2>';
   $array['status'] = 'error';
   echo(json_encode($array));
+ }
+else
+ {
+  $title = '<h1 class="post__title">';
+  $c = our_find($title,$homepage,"</span>");
+  echo($c.'</br>');
+  $array['title'] = $c;
 
+  $text_1 = '<div class="content html_format">';
+  $c = our_find($text_1,$homepage,"</div>");
+  $a = mb_strpos($c,".");
+  $c = mb_substr($c,0,$a+1);
+  echo($c.'</br>');
+  $array['text'] = $c;
+
+  $date_pub = '<span class="post__time_published">';
+  $c = our_find($date_pub,$homepage,"</span>");
+  echo($c.'</br>');
+  $array['date'] = $c;
+
+  $text_1 = '<div class="views-count_post"';
+  $c = our_find($text_1,$homepage,'</div>');
+  echo($c.'</br>');
+  $array['rating'] = $c;
+
+  $text_1 = '<span class="voting-wjt__counter-score js-score"';
+  $c = our_find($text_1,$homepage,'</div>');
+  echo($c.'</br>');
+  $array['stars'] = $c;
+
+  $text_1 = '<span class="favorite-wjt__counter js-favs_count"';
+  $c = our_find($text_1,$homepage,'</div>');
+  echo($c.'</br>');
+  $array['views'] = $c;
+
+  $text_1 = '<div class="post__tags">';
+  $c = our_find($text_1,$homepage,'</div>');
+  $c = str_replace("\n", NULL, $c);
+  echo($c.'</br></br>');
+  $array['tags'] = explode(",",$c);
+
+  echo(json_encode($array,JSON_UNESCAPED_UNICODE));
 }
-else {
-
-$title = '<h1 class="post__title">';
-$c = our_find($title,$homepage,"</span>");
-echo($c.'</br>');
-$array['title'] = $c;
-
-$text_1 = '<div class="content html_format">';
-$c = our_find($text_1,$homepage,"</div>");
-$a = mb_strpos($c,".");
-$c = mb_substr($c,0,$a+1);
-echo($c.'</br>');
-$array['text'] = $c;
-
-$date_pub = '<span class="post__time_published">';
-$c = our_find($date_pub,$homepage,"</span>");
-echo($c.'</br>');
-$array['date'] = $c;
-
-$text_1 = '<div class="views-count_post"';
-$c = our_find($text_1,$homepage,'</div>');
-echo($c.'</br>');
-$array['rating'] = $c;
-
-$text_1 = '<span class="voting-wjt__counter-score js-score"';
-$c = our_find($text_1,$homepage,'</div>');
-echo($c.'</br>');
-$array['stars'] = $c;
-
-$text_1 = '<span class="favorite-wjt__counter js-favs_count"';
-$c = our_find($text_1,$homepage,'</div>');
-echo($c.'</br>');
-$array['views'] = $c;
-
-$text_1 = '<div class="post__tags">';
-$c = our_find($text_1,$homepage,'</div>');
-$c = str_replace("\n", NULL, $c);
-echo($c.'</br></br>');
-$array['tags'] = explode(",",$c);
-
-echo(json_encode($array,JSON_UNESCAPED_UNICODE));
-
-}
-
 ?>
